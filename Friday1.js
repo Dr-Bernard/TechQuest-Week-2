90e4ccfe76df4e998fd36985345b0b11d582737b
Dr-Bernard <drsunny3030@gmail.com>
2023-04-05 13:15:26 +0100

    commit2

---
+++ Friday1.js
@@ -0,0 +1,21 @@
+let check;
+const battery = null;
+const num = 56
+const name = “introduction to data types”;
+const sym = Symbol();
+const happy = true;
+const obj = {a:1, b:"bee”, c:3};
+const arr = [1,2,4,'a’,null,5];
+const myFunct = function (){
+console.log(“my function”);
+}
+
+console.log(typeof(check));
+console.log(typeof(battery));
+console.log(typeof(num));
+console.log(typeof(name));
+console.log(typeof(sym));
+console.log(typeof(happy));
+console.log(typeof(obj));
+console.log(typeof(arrays));
+console.log(typeof(myFunct));
---
+++ Sunday1.js
@@ -0,0 +1,8 @@
+let Ada = "20,000"
+let Nne = "5,000"
+let Obi = "Ada" + "Nne"
+console.log("Obi")
+
+
+var num = 1000 + 800
+console.log(num)
